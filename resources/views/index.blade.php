f163adc579ffc766de2b64605c51ed171ecafaca
DESKTOP-PT1O1NO\Admin <nguyekhanhdanh170201@gmail.com>
2023-12-23 02:21:56 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,499 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+  <title>Lugx Gaming Shop HTML5 Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/templatemo-lugx-gaming.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/owl.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+  
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+  <!--
+
+TemplateMo 589 lugx gaming
+
+https://templatemo.com/tm-589-lugx-gaming
+
+-->
 </head>
+
 <body>
-    <h2>LARAVEL INTRO</h2>
-    <hr>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img src="assets/images/logo.png" alt="" style="width: 158px;">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="index.html" class="active">Home</a></li>
+              <li><a href="shop.html">Our Shop</a></li>
+              <li><a href="product-details.html">Product Details</a></li>
+              <li><a href="contact.html">Contact Us</a></li>
+            </ul>
+
+            <ul class="nav">
+              <li><a href="#"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+              <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+              <form id="search" action="#">
+                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
+                  onkeypress="handle" />
+              </form>
+            </ul>
+
+
+            <a class='menu-trigger'>
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="caption header-text">
+            <h6>Welcome to lugx</h6>
+            <h2>BEST GAMING SITE EVER!</h2>
+            <p>LUGX Gaming is free Bootstrap 5 HTML CSS website template for your gaming websites. You can download and
+              use this layout for commercial purposes. Please tell your friends about TemplateMo.</p>
+            <div class="search-input">
+              <form id="search" action="#">
+                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
+                  onkeypress="handle" />
+                <button role="button">Search Now</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 offset-lg-2">
+          <div class="right-image">
+            <img src="assets/images/banner-image.jpg" alt="">
+            <span class="price">$22</span>
+            <span class="offer">-40%</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="features">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <a href="#">
+            <div class="item">
+              <h4>PC</h4>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="#">
+            <div class="item">
+              <h4>NINTENDO</h4>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="#">
+            <div class="item">
+
+              <h4>XBOX 360</h4>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="#">
+            <div class="item">
+              <h4>PLAY STATION</h4>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section trending">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h6>Trending</h6>
+            <h2>Trending Games</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="main-button">
+            <a href="shop.html">View All</a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/trending-01.jpg" alt=""></a>
+              <span class="price"><em>$28</em>$20</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Action</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/trending-02.jpg" alt=""></a>
+              <span class="price">$44</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Action</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/trending-03.jpg" alt=""></a>
+              <span class="price"><em>$64</em>$44</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Action</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/trending-04.jpg" alt=""></a>
+              <span class="price">$32</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Action</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section most-played">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h6>TOP GAMES</h6>
+            <h2>Most Played</h2>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="main-button">
+            <a href="shop.html">View All</a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-01.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-02.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-03.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-04.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-05.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/top-game-06.jpg" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section categories">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <div class="section-heading">
+            <h6>Categories</h6>
+            <h2>Top Categories</h2>
+          </div>
+        </div>
+        <div class="swiper mySwiper"style="padding-left: 70px;" >
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class=" col-sm-6 col-xs-12" >
+                <div class="item">
+                  <h4>Action</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>RPG</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>RTS</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Indie</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Fighting</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Racing</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Adventure</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Sport</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Horror</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Visual Novel</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>2 10</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="col-lg col-sm-6 col-xs-12">
+                <div class="item">
+                  <h4>Shoting</h4>
+                  <div class="thumb">
+                    <a href="product-details.html"><img src="assets/images/categories-01.jpg" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="swiper-button-next"></div>
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <!-- Initialize Swiper -->
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 5,
+      
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  </script>
+
+  
+
+  <footer>
+    <div class="container">
+      <div class="col-lg-12">
+        <p>Copyright © 2048 LUGX Gaming Company. All rights reserved. &nbsp;&nbsp; <a rel="nofollow"
+            href="https://templatemo.com" target="_blank">Design: TemplateMo</a></p>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+
+
+  <script src="{{ asset('js/isotope.min.js') }}"></script>
+  <script src="{{ asset('js/owl-carousel.js') }}"></script>
+  <script src="{{ asset('js/counter.js') }}"></script>
+  <script src="{{ asset('js/custom.js') }}"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+
 Route::get('/', function () {
 return view('index');
 });
+
+
 Route::get('product/index', [ProductController::class, 'index']);
+
 Route::get('product/create', [ProductController::class, 'create']);
 Route::post('product/postCreate', [ProductController::class, 'postCreate']);
+
 Route::get('product/update/{id}', [ProductController::class, 'update']);
 Route::post('product/postUpdate/{id}', [ProductController::class, 'postUpdate']);
+
 Route::get('product/delete/{id}', [ProductController::class, 'delete']);
